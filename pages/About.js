a68bece538f0365e89b4468ba8cd3a3514a7495b
azufb <curiousazunas@gmail.com>
2022-02-14 22:24:46 +0900

    Aboutページ準備

--- pages/About.js
+++ pages/About.js
@@ -1,9 +1,58 @@
 import Layout from '../components/Layout';
+import styles from '../styles/About.module.css';
 
 export default function About() {
     return (
         <Layout>
-            <h1>Aboutページ</h1>
+            <div className={styles.contents}>
+                <h1>About</h1>
+                <p>
+                    <span>
+                        1998.02<br />
+                        兵庫県に生まれる。
+                    </span>
+                </p>
+
+                <p>
+                    <span>
+                        2016.03<br />
+                        高校卒業。
+                    </span>
+                </p>
+
+                <p>
+                    <span>
+                        2016.04<br />
+                        大学入学。
+                    </span>
+                </p>
+
+                <p>
+                    <span>
+                        2020.03<br />
+                        大学卒業。
+                    </span>
+                </p>
+
+                <p>
+                    <span>
+                        2020.10<br />
+                        J.B.Goode株式会社に入社。(アルバイト)
+                    </span>
+                </p>
+
+                <p>
+                    <span>
+                        2021.10<br />
+                        株式会社ルートゼロに入社。
+                    </span>
+                </p>
+                
+                <h1>Skills</h1>
+                <div></div>
+                <h1>Certification</h1>
+                <div></div>
+            </div>
         </Layout>
     )
 }
