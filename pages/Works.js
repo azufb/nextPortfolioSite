ede4b4631adb447c8bd4fc69f03f2cdbcc488102
azufb <curiousazunas@gmail.com>
2022-02-18 21:02:55 +0900

    Works一覧ページ作成

--- pages/Works.js
+++ pages/Works.js
@@ -1,9 +1,37 @@
 import Layout from '../components/Layout';
+import { client } from '../libs/client';
+import styles from '../styles/Works.module.css';
 
-export default function Works() {
+export default function Works({ data }) {
     return (
         <Layout>
-            <h1>Worksページ</h1>
+            <div className={styles.contents}>
+                <h1>Works</h1>
+                <div className={styles.cards}>
+                    {data.map((work) => (
+                        <div key={work.id} className={styles.card}>
+                            <img src={work.image.url.concat('?fit=fill')} alt="今日のやること" />
+                            <div className={styles.cardContents}>
+                                <p className={styles.appName}>{work.name}</p>
+                                <p className={styles.finishedDate}>{new Date(work.date).toLocaleDateString()}</p>
+                                <p className={styles.tag}>#{work.tag}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </Layout>
     )
+}
+
+export const getStaticProps = async () => {
+    const data = await client.get({ endpoint: "works" });
+
+    return {
+        props: {
+            data: (data.contents).sort((a, b) => {
+                return new Date(b.date) - new Date(a.date)
+            })
+        }
+    }
 }
