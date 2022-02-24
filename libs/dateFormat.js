3134dadeb815d45afb7ed5a08b47c1e7201898be
azufb <curiousazunas@gmail.com>
2022-02-24 21:14:31 +0900

    不要なコード削除

--- libs/dateFormat.js
+++ libs/dateFormat.js
@@ -1,6 +1,5 @@
 export const formatDate = (...value) => {
     const finishedDate = new Date(...value.splice(","));
-    console.log(finishedDate);
     let formattedDate = finishedDate.getFullYear() + "/" + ("0" + (finishedDate.getMonth() + 1)).slice(-2) + "/" + ("0" + (finishedDate.getDate())).slice(-2);
     return formattedDate;
 }
